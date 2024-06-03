e276490e39c93c5406a5d65e6ccc1f1f01b883be
Amir Vaxman <avaxman@gmail.com>
2024-06-03 23:39:13 +0100

    Singularities working.

--- solutions/Fields2D.py
+++ solutions/Fields2D.py
@@ -2,7 +2,7 @@ import os
 import polyscope as ps
 import polyscope.imgui as psim
 import numpy as np
-from scipy.sparse import coo_matrix
+from scipy.sparse import coo_matrix, csc_matrix
 from scipy.sparse.linalg import spsolve
 import cmath
 
@@ -45,7 +45,7 @@ def compute_face_quantities(vertices, faces):
     normals /= (2.0 * faceAreas[:, np.newaxis])
 
     basisX = vectors1
-    basisX = basisX / np.linalg.norm(basisX, axis=1,  keepdims=True)
+    basisX = basisX / np.linalg.norm(basisX, axis=1, keepdims=True)
     basisY = np.cross(normals, basisX)
     return normals, faceAreas, basisX, basisY
 
@@ -135,10 +135,50 @@ def power_to_extrinsic(N, powerField, inFaces, basisX, basisY):
     extField = np.zeros([powerField.shape[0], 3 * N])
     repField = np.exp(np.log(powerField) / N)
     for i in range(0, N):
-        currField = repField * cmath.exp(2*i * cmath.pi / N * 1j)
+        currField = repField * cmath.exp(2 * i * cmath.pi / N * 1j)
         extField[:, 3 * i:3 * i + 3] = np.real(currField) * basisX + np.imag(currField) * basisY
 
     return extField
+
+
+def compute_angle_defect(vertices, faces):
+    vi = vertices[faces[:, 0], :]
+    vj = vertices[faces[:, 1], :]
+    vk = vertices[faces[:, 2], :]
+
+    cosijk = np.sum((vi - vj) * (vk - vj), axis=1) / np.linalg.norm(vi - vj, axis=1) / np.linalg.norm(vk - vj, axis=1)
+    cosjki = np.sum((vj - vk) * (vi - vk), axis=1) / np.linalg.norm(vj - vk, axis=1) / np.linalg.norm(vi - vk, axis=1)
+    coskij = np.sum((vk - vi) * (vj - vi), axis=1) / np.linalg.norm(vk - vi, axis=1) / np.linalg.norm(vj - vi, axis=1)
+
+    cornerAngles = np.arccos(np.column_stack((coskij, cosijk, cosjki)))
+
+    angleDefect = 2 * np.pi - accumarray(faces, cornerAngles)
+
+    return angleDefect
+
+
+def get_singularities(N, powerField, vertices, faces, edges, basisX, basisY):
+    # angle defect
+    K = compute_angle_defect(vertices, faces).reshape(-1,1)
+
+    # d0 operator
+    rows = np.arange(0, edges.shape[0])
+    rows = np.tile(rows[:, np.newaxis], 2)
+    values = np.full((edges.shape[0], 2), [-1, 1])
+    d0 = csc_matrix((values.flatten(), (rows.flatten(), edges.flatten())), shape=(edges.shape[0], vertices.shape[0]))
+
+    # computing effort
+    edgeVectors = vertices[edges[:, 1], :] - vertices[edges[:, 0], :]
+    edgeVectorsFace1 = extrinsic_to_power(N, edgeVectors, EF[:, 0], basisX, basisY)
+    edgeVectorsFace2 = extrinsic_to_power(N, edgeVectors, EF[:, 2], basisX, basisY)
+
+    expEffort = (powerField[EF[:, 2]] * np.conj(edgeVectorsFace2)) / (powerField[EF[:, 0]] * np.conj(edgeVectorsFace1))
+    effort = np.imag(np.log(expEffort))
+
+    indices = (d0.T*effort + N*K)/(2*np.pi)
+    singVertices = np.column_stack(np.nonzero(np.round(indices)))[:, 0]
+    singIndices = np.round(indices)[singVertices]
+    return singVertices, singIndices
 
 
 def interpolate_power_field(N, constField2D, constFaces, vertices, faces, edges, EF, basisX, basisY):
@@ -148,7 +188,7 @@ def interpolate_power_field(N, constField2D, constFaces, vertices, faces, edges,
     edgeVectorsFace2 = extrinsic_to_power(N, edgeVectors, EF[:, 2], basisX, basisY)
 
     # preparing constraints and linear system
-    crossFieldConst = constField2D #np.exp(np.log(constField2D) * 4)
+    crossFieldConst = constField2D  # np.exp(np.log(constField2D) * 4)
     rows = np.column_stack((np.arange(0, edges.shape[0]), np.arange(0, edges.shape[0])))
     cols = EF[:, [0, 2]]
     values = np.column_stack([-np.conj(edgeVectorsFace1), np.conj(edgeVectorsFace2)])
@@ -158,9 +198,9 @@ def interpolate_power_field(N, constField2D, constFaces, vertices, faces, edges,
     varFaces = [i for i in range(A.shape[1]) if i not in constFaces]
     AVar = A[:, varFaces]
     b = -AConst * crossFieldConst
-    crossFieldFull = np.zeros([faces.shape[0],1], dtype=np.complex128)
+    crossFieldFull = np.zeros([faces.shape[0], 1], dtype=np.complex128)
     crossFieldFull[constFaces] = crossFieldConst
-    crossFieldFull[varFaces] = spsolve(np.dot(np.conj(AVar.T), AVar), np.conj(AVar.T).dot(b)).reshape(-1,1)
+    crossFieldFull[varFaces] = spsolve(np.dot(np.conj(AVar.T), AVar), np.conj(AVar.T).dot(b)).reshape(-1, 1)
 
     return crossFieldFull
 
@@ -174,22 +214,28 @@ if __name__ == '__main__':
 
     halfedges, edges, edgeBoundMask, boundVertices, EH, EF = compute_edge_list(vertices, faces)
 
+    N = 4
     constFaces = [1, 1000, 2000, 3000]
 
     # set to the first edge of each face
     constField3D = vertices[faces[constFaces, 2], :] - vertices[faces[constFaces, 1], :]
-    constField3D /= np.linalg.norm(constField3D, axis=1,  keepdims=True)
+    constField3D /= np.linalg.norm(constField3D, axis=1, keepdims=True)
 
     # in complex coordinates
-    constField2D = extrinsic_to_power(4, constField3D, constFaces, basisX, basisY)
+    constField2D = extrinsic_to_power(N, constField3D, constFaces, basisX, basisY)
 
-    crossField = interpolate_power_field(4, constField2D, constFaces, vertices, faces, edges, EF, basisX, basisY)
+    crossField = interpolate_power_field(N, constField2D, constFaces, vertices, faces, edges, EF, basisX, basisY)
 
-    extField = power_to_extrinsic(4, crossField, range(0, faces.shape[0]), basisX, basisY)
+    extField = power_to_extrinsic(N, crossField, range(0, faces.shape[0]), basisX, basisY)
 
     ps_mesh = ps.register_surface_mesh("Hello World Mesh", vertices, faces)
 
     for i in range(0, 4):
         ps_mesh.add_vector_quantity("field" + str(i), extField[:, 3 * i:3 * i + 3], defined_on='faces')
+
+    singVertices, singIndices = get_singularities(4, crossField, vertices, faces, edges, basisX, basisY)
+
+    ps_cloud = ps.register_point_cloud("Singularities", vertices[singVertices, :])
+    ps_cloud.add_scalar_quantity("Indices", singIndices.flatten(), vminmax=(-N, N), enabled=True)
 
     ps.show()
